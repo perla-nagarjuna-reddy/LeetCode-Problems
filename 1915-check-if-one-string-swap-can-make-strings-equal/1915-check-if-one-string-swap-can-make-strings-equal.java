6c9a0415e67a28374b0fb3aa940d0c9502eb6966
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-07-31 23:01:44 +0530

    Time: 4 ms (9.45%), Space: 41.4 MB (72.97%) - LeetHub

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        HashMap<Character,Integer> hm = new HashMap<>();
+        for(int i = 0;i<s1.length();i++){
+            if(hm.containsKey(s1.charAt(i))){
+                hm.put(s1.charAt(i),hm.get(s1.charAt(i))+1);
+            }
+            else{
+                hm.put(s1.charAt(i),1);
+            }
+        }
+        int update = 0;
+        for(int i = 0;i<s2.length();i++){
+            if(!hm.containsKey(s2.charAt(i))){
+                return false;
+            }
+            if(hm.get(s2.charAt(i)) > 0){
+                hm.put(s2.charAt(i),hm.get(s2.charAt(i)) - 1);
+                if(s1.charAt(i) != s2.charAt(i)){
+                    update+=1;
+                }
+            }
+            if(hm.get(s2.charAt(i)) == 0){
+                hm.remove(s2.charAt(i));
+            }
+            
+        }
+        return update == 2 || update == 0;
+    }
+}
