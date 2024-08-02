1c786cf69f042734bdd7f90208e136c9427f6867
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-08-02 23:13:29 +0530

    Time: 0 ms (100%), Space: 51.9 MB (8.17%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.java
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.java
@@ -1,35 +1,14 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
-    int sum = 0;
     public int rangeSumBST(TreeNode root, int low, int high) {
-       helper(root,low,high);
-       return sum;
-    }
-
-    public void helper(TreeNode root, int low, int high){
-
-        if(root == null){
-            return;
-        }
-        if(root.val >= low && root.val <= high){
-            System.out.println(root.val);
-            sum+=root.val;
-        }
-        helper(root.left, low, high);
-        helper(root.right, low, high);
+        if(root == null)
+            return 0;
+        
+        if(root.val >= low && root.val <= high)
+            return rangeSumBST(root.left, low, high) + root.val + rangeSumBST(root.right, low, high);
+        else if (root.val > high)
+            return rangeSumBST(root.left, low, high);
+        else 
+            return rangeSumBST(root.right, low, high);
     }
 }
