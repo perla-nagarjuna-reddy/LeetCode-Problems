bf9f6891ee6fb423955829ba62e2451542b54c28
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-08-04 13:23:13 +0530

    Time: 0 ms (100%), Space: 44.3 MB (59.4%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,27 +10,42 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        ListNode oddNode = new ListNode(-1);
-        ListNode evenNode = new ListNode(-1);
-        ListNode current1 = oddNode;
-        ListNode current2 = evenNode;
-        ListNode temp = head;
-        int count = 1;
-        while(temp != null){
-            if(count % 2 == 1){
-                ListNode node = new ListNode(temp.val);
-                current1.next = node;
-                current1 = node;
-            }
-            else{
-                ListNode node = new ListNode(temp.val);
-                current2.next = node;
-                current2 = node;
-            }
-            temp = temp.next;
-            count++;
+        if(head == null || head.next == null) return head;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = head.next;
+
+        while(even != null && even.next != null){
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
         }
-        current1.next = evenNode.next;
-        return oddNode.next;
+
+        odd.next = evenHead;
+        return head;
     }
 }
+
+// ListNode oddNode = new ListNode(-1);
+//         ListNode evenNode = new ListNode(-1);
+//         ListNode current1 = oddNode;
+//         ListNode current2 = evenNode;
+//         ListNode temp = head;
+//         int count = 1;
+//         while(temp != null){
+//             if(count % 2 == 1){
+//                 ListNode node = new ListNode(temp.val);
+//                 current1.next = node;
+//                 current1 = node;
+//             }
+//             else{
+//                 ListNode node = new ListNode(temp.val);
+//                 current2.next = node;
+//                 current2 = node;
+//             }
+//             temp = temp.next;
+//             count++;
+//         }
+//         current1.next = evenNode.next;
+//         return oddNode.next;
