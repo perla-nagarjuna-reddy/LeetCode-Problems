762701845271bb4b22a6884f5ee0095a0b30ac48
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-08-04 16:23:57 +0530

    Time: 0 ms (100%), Space: 41.4 MB (83.37%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,31 +10,46 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int size = sizeOfLL(head);
-        int toBeDeletedNode = size - n + 1;
-        if(size == n){
-            head = head.next;
-            return head;
+        
+        ListNode fast = head;
+        for(int i=0;i<n;i++){
+            fast = fast.next;
         }
-        ListNode temp = head,prev = null;
-        int cnt = 1;
-        while(temp != null){
-            if(cnt == toBeDeletedNode) break;
-            cnt++;
-            prev = temp;
-            temp = temp.next;
+        if(fast == null) return head.next;
+        ListNode slow = head;
+
+        while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
         }
-        prev.next = prev.next.next;
+        slow.next = slow.next.next;
         return head;
     }
-
-    public int sizeOfLL(ListNode head){
-        ListNode temp = head;
-        int count = 0;
-        while(temp != null){
-            count++;
-            temp = temp.next;
-        }
-        return count;
-    }
 }
+
+// int size = sizeOfLL(head);
+//         int toBeDeletedNode = size - n + 1;
+//         if(size == n){
+//             head = head.next;
+//             return head;
+//         }
+//         ListNode temp = head,prev = null;
+//         int cnt = 1;
+//         while(temp != null){
+//             if(cnt == toBeDeletedNode) break;
+//             cnt++;
+//             prev = temp;
+//             temp = temp.next;
+//         }
+//         prev.next = prev.next.next;
+//         return head;
+//     }
+
+//     public int sizeOfLL(ListNode head){
+//         ListNode temp = head;
+//         int count = 0;
+//         while(temp != null){
+//             count++;
+//             temp = temp.next;
+//         }
+//         return count;
