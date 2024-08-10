a016768529072990e891ce41b38774b0a84675ee
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-08-10 21:48:57 +0530

    Time: 0 ms (100%), Space: 44.2 MB (26.11%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -12,33 +12,48 @@ class Solution {
     public ListNode deleteDuplicates(ListNode head) {
 
         if(head == null || head.next == null) return head;
-        ArrayList<Integer> arr = new ArrayList<>();
-
-        ListNode temp = head;
-
-        while(temp != null){
-            if(arr.contains(temp.val)){
-                temp = temp.next;
-                continue;
+        ListNode temp = head; // Initialize the pointer to traverse the list
+        
+        while (temp.next != null) { // Continue until the end of the list
+            if (temp.val == temp.next.val) { // Check if current node is a duplicate
+                temp.next = temp.next.next; // Skip the duplicate node
+            } else {
+                temp = temp.next; // Move to the next node
             }
-            else{
-                arr.add(temp.val);
-            }
-            temp = temp.next;
         }
-
-        return createLL(arr);
+        
+        return head; // Return the modified list
     }
-
-    public ListNode createLL(ArrayList<Integer> arr){
-        ListNode head = new ListNode(arr.get(0));
-        ListNode mover = head;
-        for(int i=1;i<arr.size();i++){
-            ListNode temp = new ListNode(arr.get(i));
-            mover.next = temp;
-            mover = temp;
-        }
-
-        return head;
-    }
+       
 }
+
+
+//  ArrayList<Integer> arr = new ArrayList<>();
+
+//         ListNode temp = head;
+
+//         while(temp != null){
+//             if(arr.contains(temp.val)){
+//                 temp = temp.next;
+//                 continue;
+//             }
+//             else{
+//                 arr.add(temp.val);
+//             }
+//             temp = temp.next;
+//         }
+
+//         return createLL(arr);
+//     }
+
+//     public ListNode createLL(ArrayList<Integer> arr){
+//         ListNode head = new ListNode(arr.get(0));
+//         ListNode mover = head;
+//         for(int i=1;i<arr.size();i++){
+//             ListNode temp = new ListNode(arr.get(i));
+//             mover.next = temp;
+//             mover = temp;
+//         }
+
+//         return head;
+//     }
