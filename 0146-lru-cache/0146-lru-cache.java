e5287174e612701f0b9fac7d909718e3c7598948
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-08-28 23:45:54 +0530

    Time: 44 ms (76.2%), Space: 112.6 MB (78.93%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,50 +1,110 @@
-class Pair{
+class Node{
     int key;
     int val;
-    Pair(int key,int val){
+    Node next;
+    Node prev;
+    Node(int key,int val){
         this.key = key;
         this.val = val;
     }
 }
+
 class LRUCache {
-    HashMap<Integer,Pair> map;
-    Deque<Pair> q;
-    int c;
+    int cap ;
+    HashMap<Integer,Node> map;
+    Node head = new Node(-1,-1);
+    Node tail = new Node(-1,-1);
     public LRUCache(int capacity) {
-        c = capacity;
+        cap = capacity;
         map = new HashMap<>();
-        q = new LinkedList<>();
+        head.next = tail;
+        tail.prev = head;
     }
     
     public int get(int key) {
-        if(map.containsKey(key)){
-            Pair pair = map.get(key);
-            q.remove(pair);
-            q.addFirst(pair);
-            return map.get(key).val;
+        if(map.containsKey(key)) {
+            Node temp = map.get(key);
+            deleteNode(temp);
+            insertAfterHead(temp);
+            return temp.val;
         }
-
-        return -1;
+        else{
+            return -1;
+        }
+        
     }
     
+    
     public void put(int key, int value) {
-        if (map.containsKey(key)) {
-            Pair pair = map.get(key);
-            q.remove(pair); // Remove the old pair from the queue
-            pair.val = value; // Update the value
-            q.addFirst(pair); // Add it back to the front
-        } else {
-            Pair pair = new Pair(key, value);
-            if (q.size() >= c) {
-                Pair last = q.pollLast();
-                map.remove(last.key);
+        if(map.containsKey(key)){
+            Node temp = map.get(key);
+            temp.val = value;
+            deleteNode(temp);
+            insertAfterHead(temp);
+        }else{
+            if(map.size() == cap){
+                Node node = tail.prev;
+                map.remove(node.key);
+                deleteNode(node);
             }
-            q.addFirst(pair);
-            map.put(key, pair);
+            Node newNode = new Node(key,value);
+            map.put(key,newNode);
+            insertAfterHead(newNode);
         }
-
+    }
+    public void deleteNode(Node temp){
+        Node prevNode = temp.prev;
+        Node afterNode = temp.next;
+        prevNode.next = afterNode;
+        afterNode.prev = prevNode;
+    }
+    public void insertAfterHead(Node temp){
+        Node afterNode = head.next;
+        head.next = temp;
+        temp.prev = head;
+        temp.next = afterNode;
+        afterNode.prev = temp;
     }
 }
+// class LRUCache {
+//     HashMap<Integer,Pair> map;
+//     Deque<Pair> q;
+//     int c;
+//     public LRUCache(int capacity) {
+//         c = capacity;
+//         map = new HashMap<>();
+//         q = new LinkedList<>();
+//     }
+    
+//     public int get(int key) {
+//         if(map.containsKey(key)){
+//             Pair pair = map.get(key);
+//             q.remove(pair);
+//             q.addFirst(pair);
+//             return map.get(key).val;
+//         }
+
+//         return -1;
+//     }
+    
+//     public void put(int key, int value) {
+//         if (map.containsKey(key)) {
+//             Pair pair = map.get(key);
+//             q.remove(pair); 
+//             pair.val = value;
+//             q.addFirst(pair);
+//         } else {
+//             Pair pair = new Pair(key, value);
+//             if (q.size() >= c) {
+//                 Pair last = q.pollLast();
+//                 map.remove(last.key);
+//             }
+//             q.addFirst(pair);
+//             map.put(key, pair);
+//         }
+
+//     }
+// }
 
 /**
  * Your LRUCache object will be instantiated and called as such:
