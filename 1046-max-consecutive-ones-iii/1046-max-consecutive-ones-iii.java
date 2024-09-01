c766e973be9131569c5d6a973d4b6ed94e33b843
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-09-01 13:44:48 +0530

    Time: 4 ms (25.6%), Space: 47.3 MB (17.27%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -1,7 +1,6 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
         int max = 0;
-
         int l=0;
         int r = 0;
         int cnt = 0;
@@ -9,7 +8,7 @@ class Solution {
             if(nums[r] == 0){
                 cnt++;
             }
-            while(cnt > k){
+            if(cnt > k){
                 if(nums[l] == 0){
                     cnt--;
                 }
