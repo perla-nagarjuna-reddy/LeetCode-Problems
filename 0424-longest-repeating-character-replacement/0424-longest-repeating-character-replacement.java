d19119730e837f7a6092c20f84d3355a19006c7f
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-09-02 23:19:23 +0530

    Time: 6 ms (92.25%), Space: 42.2 MB (88.11%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] hash = new int[26];
+
+        int l = 0;
+        int r = 0;
+        int maxLength = 0;
+        int maxFrequency = 0;
+        while(r < s.length()){
+            hash[s.charAt(r)-'A']++;
+            maxFrequency = Math.max(maxFrequency, hash[s.charAt(r)-'A']);
+            while((r-l+1) - maxFrequency > k){
+                hash[s.charAt(l)-'A']--;
+                maxFrequency = 0;
+
+                for(int i=0;i<=25;i++){
+                    maxFrequency = Math.max(maxFrequency,hash[i]);
+                }
+                l++;
+            }
+
+            if((r-l+1)-maxFrequency <=k){
+                maxLength = Math.max(maxLength,r-l+1);
+            }
+            r++;
+        } 
+
+        return maxLength;
+    }
+}
