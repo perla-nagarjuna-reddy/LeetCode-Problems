e6135322ba2220aa8a36bd8a03a71c8f77847a7e
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-09-10 22:37:52 +0530

    Time: 1 ms (99.82%), Space: 46.5 MB (6.37%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int num = 0;
+        for(int val : nums){
+            num = num ^ val;
+        }
+
+        return num;
+    }
+}
