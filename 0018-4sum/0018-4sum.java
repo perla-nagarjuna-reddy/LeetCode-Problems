4eb76713dd32071bb41f55d428006a9bdbe27848
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-09-16 22:44:09 +0530

    Time: 18 ms (60.25%), Space: 43.9 MB (61.65%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,27 +1,37 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n = nums.length;
+        Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-        Set<List<Integer>> set = new HashSet<>();
-
         for(int i=0;i<n;i++){
+
+            if(i > 0 && nums[i] == nums[i-1])continue;
             for(int j=i+1;j<n;j++){
-                Set<Long> st = new HashSet<>();
-                for(int k=j+1;k<n;k++){
-                    long pend = (long)target - ((long)nums[i] + (long)nums[j] + (long)nums[k]);
-                    if(st.contains(pend)){
-                        List<Integer> temp = Arrays.asList(nums[i],nums[j],nums[k],(int)pend);
-                        Collections.sort(temp);
-                        set.add(temp);
+                if(j > i+1 && nums[j] == nums[j-1])continue;
+                int k=j+1;
+                int l = n-1;
+                while(k < l){
+                   long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    // System.out.println(sum);
+                    if(sum == target){
+                        ans.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                        k++;
+                        l--;
+                        while(k < l && nums[k] == nums[k-1]) k++;
+                        while(k < l && nums[l] == nums[l+1]) l--;
                     }
-                    st.add((long)nums[k]);
+                    else if(sum < target){
+                        k++;
+                    }
+                    else{
+                        l--;
+                    }
                 }
             }
         }
-        for(List<Integer> list : set){
-            ans.add(list);
-        }
-
         return ans;
     }
 }
