21e090392fd7155da219eb7192bbe23df61178b0
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-09-23 22:38:49 +0530

    Time: 3 ms (77.72%), Space: 43.2 MB (58.43%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -2,31 +2,23 @@ class Solution {
     public List<List<Integer>> combinationSum2(int[] arr, int target) {
         Arrays.sort(arr);
         List<List<Integer>> ans = new ArrayList<>();
-        Set<List<Integer>> set = new HashSet<>();
         List<Integer> res =  new ArrayList<>();
-        findCombinations(arr,0,arr.length,target,res,ans,set); 
+        findCombinations(arr,0,target,res,ans); 
         return ans;
     }
 
-    public void findCombinations(int[] arr,int ind,int n,int target,List<Integer> res,List<List<Integer>> ans,Set<List<Integer>> set){
-        if(ind == n){
+    public void findCombinations(int[] arr,int ind,int target,List<Integer> res,List<List<Integer>> ans){
             if(target == 0){
-                // Collections.sort(res);
-                if(!set.contains(res)){
-                    ans.add(new ArrayList<>(res));
-                }
-                set.add(res);
+                ans.add(new ArrayList<>(res));
+                return;
             }
 
-            return;
-        }
-
-        if(arr[ind] <= target){
-            res.add(arr[ind]);
-            findCombinations(arr,ind+1,n,target - arr[ind],res,ans,set);
-            while(ind < n-1 && arr[ind] == arr[ind+1]) ind++;
-            res.remove(res.size() - 1);
-        }
-        findCombinations(arr,ind+1,n,target,res,ans,set);
+            for(int i=ind;i<arr.length;i++){
+                if(i > ind && arr[i] == arr[i-1]) continue;
+                if(arr[ind] > target) break;
+                res.add(arr[i]);
+                findCombinations(arr,i+1,target - arr[i],res,ans);
+                res.remove(res.size() - 1);
+            }
     }
 }
