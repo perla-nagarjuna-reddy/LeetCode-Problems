3b4beeb9bd41c0ac06f7f195419bb3d0813fb38f
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-09-28 00:22:38 +0530

    Time: 0 ms (100%), Space: 39.7 MB (98.24%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,12 +1,19 @@
 class Solution {
     public int fib(int n) {
-        return findNthfib(n);
-    }
-
-    public int findNthfib(int n){
-        if(n <= 1){
-            return n;
+        if(n == 1){
+            return 1;
         }
-        return findNthfib(n-1)+findNthfib(n-2);
+        int a = 0;
+        int b = 1;
+
+        int c = 0;
+
+        for(int i=1;i<n;i++){
+            c = a+b;
+            a = b;
+            b = c;
+        }
+
+        return c;
     }
 }
