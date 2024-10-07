c833567e6af4bfbd4353d438c6077248a255acd4
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-10-07 23:22:08 +0530

    Time: 17 ms (7.85%), Space: 45.3 MB (6.02%) - LeetHub

---
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minLength(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+
+            if(st.isEmpty()){
+                st.push(s.charAt(i));
+            }
+            else if(!st.isEmpty() && (String.valueOf(st.peek())+s.charAt(i)).equals("AB") || (String.valueOf(st.peek())+s.charAt(i)).equals("CD") ){
+                // System.out.println();
+                st.pop();
+            }
+            else{
+                st.push(s.charAt(i));
+            }
+            
+        }
+
+        return st.size();
+    }
+}
