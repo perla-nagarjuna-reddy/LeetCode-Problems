fa3bbca69c0187243712897498fbf7bfd192142f
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-10-10 22:42:00 +0530

    Time: 2 ms (78.9%), Space: 45.5 MB (57.43%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -5,6 +5,9 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(i > max) return false;
             max = Math.max(max,i+nums[i]);
+            if(max >= nums.length){
+                break;
+            }
         }
 
         return true;
