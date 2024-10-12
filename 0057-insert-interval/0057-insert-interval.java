cbb2bef6825727b4e87d0df594a5780cc7b808aa
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-10-12 23:59:29 +0530

    Time: 2 ms (39.39%), Space: 45 MB (29.46%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -3,18 +3,18 @@ class Solution {
         ArrayList<int[]> arr = new ArrayList<>();
         int i = 0;
         int n = intervals.length;
-        while(i < n && intervals[i][0] < newInterval[0]){
+        while(i < n && intervals[i][1] < newInterval[0]){
            arr.add(intervals[i]);
            i = i+1;
         }
 
-        while(i < n && intervals[i][0] < newInterval[1]){
+        while(i < n && intervals[i][0] <= newInterval[1]){
             newInterval[0] = Math.min(newInterval[0],intervals[i][0]);
             newInterval[1] = Math.max(newInterval[1],intervals[i][1]);
             i = i+1;
         }
         arr.add(newInterval);
-
+        
         while(i < n){
             arr.add(intervals[i]);
             i++;
@@ -25,33 +25,6 @@ class Solution {
         for(int[] ar : arr){
             ans[ind++] = ar;
         }
-        return merge(ans);
-    }
-    public int[][] merge(int[][] intervals) {
-        int n = intervals.length;
-
-        List<int[]> ans = new ArrayList<>();
-        int start = intervals[0][0];
-        int end  = intervals[0][1];
-
-        for(int i=1;i<intervals.length;i++){
-            int[] temp = intervals[i];
-            if(temp[0] >= start && temp[0] <= end){
-                end = Math.max(end,temp[1]);
-            }
-            else{
-                ans.add(new int[]{start,end});
-                start = intervals[i][0];
-                end = intervals[i][1];
-            }
-        }
-        ans.add(new int[]{start,end});
-        int[][] final_ans = new int[ans.size()][2];
-        int ind = 0;
-        for(int[] ls : ans){
-            final_ans[ind++] = ls;
-        }
-        return final_ans;
-
+        return ans;
     }
 }
