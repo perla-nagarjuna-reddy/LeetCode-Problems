6487b3c3f6c1f3a1f2858d33c8811c2d2e98ba5e
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-10-13 17:36:59 +0530

    Time: 81 ms (8.73%), Space: 60.4 MB (31.26%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,25 +1,14 @@
 class Solution {
-    private PriorityQueue<Integer> minHeap;
-    private int k;
     public int findKthLargest(int[] nums, int k) {
-        this.k=k;
-        minHeap=new PriorityQueue<>(new CustomerOrderComparator());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        for(int num:nums)
-        {
-            minHeap.add(num);
+        for(int i=0;i<nums.length;i++){
+            pq.add(nums[i]);
+            if(pq.size() > k){
+                pq.poll();
+            }
         }
-        for(int i=0;i<k-1;i++){
-            minHeap.poll();
-        }
-        return minHeap.poll();
-    }
-    static class CustomerOrderComparator implements Comparator<Integer> {
-
-        @Override
-        public int compare(Integer o1, Integer o2)
-        {
-            return o1 < o2 ? 1 : -1;
-        }
+        System.out.println(pq);
+        return pq.poll();
     }
 }
