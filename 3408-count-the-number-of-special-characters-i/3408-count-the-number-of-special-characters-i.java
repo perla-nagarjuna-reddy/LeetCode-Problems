47237313fa624c443763f22de14ebaeac80ae335
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-10-21 01:10:59 +0530

    Time: 1 ms (100%), Space: 42.3 MB (63.82%) - LeetHub

---
+++ 3408-count-the-number-of-special-characters-i/3408-count-the-number-of-special-characters-i.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        if(word.length() == 1){
+            return 0;
+        }
+        int[] small = new int[26];
+        int[] upper = new int[26];
+        for(int i = 0;i < word.length();i++){
+            int ch =(char)word.charAt(i);
+            // System.out.println(ch);
+            if(ch >= 97 && ch <=122){
+                small[ch-97]++;
+            }
+            else{
+                upper[ch - 65]++;
+            }
+        }
+
+        int count = 0;
+
+        for(int i = 0;i < small.length;i++){
+            if(small[i] != 0 && upper[i] != 0){
+                count++;
+            }
+        }
+
+        return count;
+
+    }
+}
