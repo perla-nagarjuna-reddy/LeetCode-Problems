728c7e6298358d175e58dedbe17a6cea25056c1a
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-11-10 22:58:43 +0530

    Time: 31 ms (44%), Space: 44.6 MB (20.74%) - LeetHub

---
+++ 1844-maximum-number-of-balls-in-a-box/1844-maximum-number-of-balls-in-a-box.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int countBalls(int lowLimit, int highLimit) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=lowLimit;i<=highLimit;i++){
+            int box = calculate(i);
+            map.put(box,map.getOrDefault(box,0)+1);
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        for(int val : map.keySet()){
+            max = Math.max(map.get(val),max);
+        }
+
+        return max;
+    }
+
+    public int calculate(int num){
+        int sum = 0;
+
+        while(num > 0){
+            int digit = num % 10;
+            sum = sum + digit;
+
+            num = num / 10; 
+        }
+
+        return sum;
+    }
+}
