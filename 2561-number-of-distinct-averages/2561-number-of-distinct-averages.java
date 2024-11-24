80e05da3739afa3987a3899999669680a37b80a2
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-11-24 22:46:30 +0530

    Time: 1 ms (100%), Space: 41.1 MB (48.51%) - LeetHub

--- 2561-number-of-distinct-averages/2561-number-of-distinct-averages.java
+++ 2561-number-of-distinct-averages/2561-number-of-distinct-averages.java
@@ -1,20 +1,16 @@
 class Solution {
     public int distinctAverages(int[] nums) {
-        Set<Float> set = new HashSet<>();
+        Arrays.sort(nums);
+        Set< Double > s = new HashSet<>();
 
-        List<Integer> lst = new ArrayList<>();
-        for(int val : nums){
-            lst.add(val);
+        int i = 0, j = nums.length - 1;
+
+        while(i < j){
+            double temp = (nums[i] + nums[j]) / 2.0;
+            s.add(temp);
+            i++;
+            j--;
         }
-        Collections.sort(lst);
-        while(lst.size() > 0){
-            int min = lst.get(0);
-            int max = lst.get(lst.size()-1);
-            float avg = (min + max) / 2.0f;
-            set.add(avg);
-            lst.remove(0);
-            lst.remove(lst.size() - 1);
-        }
-        return set.size();
+        return s.size();
     }
 }
