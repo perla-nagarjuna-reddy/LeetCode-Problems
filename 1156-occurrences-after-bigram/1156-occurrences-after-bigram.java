467690c08736b43ef358cae3b1ad4e39a1014dd7
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-12-02 22:51:53 +0530

    Time: 0 ms (100%), Space: 41.4 MB (78.55%) - LeetHub

--- 1156-occurrences-after-bigram/1156-occurrences-after-bigram.java
+++ 1156-occurrences-after-bigram/1156-occurrences-after-bigram.java
@@ -3,10 +3,8 @@ class Solution {
         String[] arr = text.split(" ");
         List<String> ans = new ArrayList<>();
         for(int i=0;i<arr.length-1;i++){
-            if(arr[i].equals(first) && arr[i+1].equals(second)){
-                if(i+2 < arr.length){
-                    ans.add(arr[i+2]);
-                }
+            if(i+2 < arr.length && arr[i].equals(first) && arr[i+1].equals(second)){
+                ans.add(arr[i+2]);
             }
         }
         String[] res = new String[ans.size()];
