8aa00438e95b0745db67f7786464f827c4b05456
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-12-07 22:39:56 +0530

    Time: 2 ms (71.21%), Space: 44.7 MB (13.83%) - LeetHub

---
+++ 2756-buy-two-chocolates/2756-buy-two-chocolates.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        // int ans = money;
+        Arrays.sort(prices);
+        if (prices.length < 2 || prices[0] + prices[1] > money) {
+            return money; 
+        }
+        return money - (prices[0] + prices[1]);
+    }
+}
