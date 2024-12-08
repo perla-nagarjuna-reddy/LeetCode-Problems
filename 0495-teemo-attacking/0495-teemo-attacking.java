aba123bb0e842aa2b10c57d401e11c761ba80fd2
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-12-08 05:49:21 +0530

    Time: 3 ms (83.06%), Space: 44.8 MB (68.34%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.java
+++ 0495-teemo-attacking/0495-teemo-attacking.java
@@ -1,9 +1,5 @@
 class Solution {
     public int findPoisonedDuration(int[] timeSeries, int duration) {
-        if(duration == 0){
-            return 0;
-        }
-        Set<Integer> set = new HashSet<>();
         int totalDuration = 0;
         for(int i=0;i < timeSeries.length - 1; i++){
             totalDuration += Math.min(duration, timeSeries[i + 1] - timeSeries[i]);
