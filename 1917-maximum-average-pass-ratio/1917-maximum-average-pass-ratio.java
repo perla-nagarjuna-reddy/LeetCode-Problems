c8327dcc6df59a24a0a71af304f6774fe0a0e88f
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2024-12-15 10:40:18 +0530

    Time: 759 ms (5.07%), Space: 100.8 MB (19.51%) - LeetHub

---
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
@@ -0,0 +1,42 @@
+class Pair{
+    int ind;
+    double avg;
+    public Pair(double avg,int ind){
+        this.ind = ind;
+        this.avg = avg;
+    }
+}
+class Solution {
+    public double maxAverageRatio(int[][] classes, int extraStudents) {
+        PriorityQueue<Pair> pq =  new PriorityQueue<>((a, b) -> Double.compare(b.avg, a.avg));
+
+        for(int i=0;i<classes.length;i++){
+            int[] arr = classes[i];
+            double currentRatio = (double) arr[0] / arr[1];
+            double newRatio = (double) (arr[0] + 1) / (arr[1] + 1);
+            double gain = newRatio - currentRatio;
+            pq.add(new Pair(gain, i));
+
+        }
+
+        while(extraStudents-- > 0){
+            Pair temp = pq.poll();
+            int i = temp.ind;
+            classes[i][0]++;
+            classes[i][1]++;
+            double currentRatio = (double) classes[i][0] / classes[i][1];
+            double newRatio = (double) (classes[i][0] + 1) / (classes[i][1] + 1);
+            double gain = newRatio - currentRatio;
+            pq.add(new Pair(gain, i));
+        }
+        for (Pair pair : pq) {
+            System.out.println(pair.avg);
+        }
+
+        double ans = 0.0;
+        for (int[] arr : classes) {
+            ans += (double) arr[0] / arr[1];
+        }
+        return ans / classes.length;
+    }
+}
