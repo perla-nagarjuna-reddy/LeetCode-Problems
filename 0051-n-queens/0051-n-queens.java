5d0661b1929182ee2f5615f2ddedc5aba37b85ae
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-02-13 00:57:41 +0530

    Time: 2 ms (89.83%), Space: 44.9 MB (55.58%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,40 +1,75 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans  = new ArrayList<>();
+        int[][] valid = new int[n][n];
         char[][] board = new char[n][n];
+        List<List<String>> ans = new ArrayList<>();
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
                 board[i][j] = '.';
             }
         }
+        int col = 0;
 
-        int[] leftRow = new int[n];
-        int[] LowerDiagonal = new int[2*n-1];
-        int[] UpperDiagonal = new int[2*n-1];
-        Solve(0,board,ans,leftRow,LowerDiagonal,UpperDiagonal,n);
+        List<String> res = new ArrayList<>();
+
+        solve(col,board,valid,n,res,ans);
+
         return ans;
     }
 
-    public void Solve(int col,char[][] board,List<List<String>> ans,int[] leftRow,int[] LowerDiagonal,int[] UpperDiagonal,int n){
+    public void solve(int col,char[][] board,int[][] valid,int n,List<String> res,List<List<String>> ans){
         if(col == n){
-            ans.add(construct(board));
+            List<String> s = construct(board);
+            ans.add(s);
             return;
         }
 
-        for(int row=0;row < n;row++){
-            if(leftRow[row] == 0 && LowerDiagonal[row+col] == 0 && UpperDiagonal[n-1+col - row] == 0){
+        for(int row = 0 ; row < n ; row++){
+
+            if(checkValidOrNot(col,row,n,valid)){
+                valid[row][col] = 1;
                 board[row][col] = 'Q';
-                leftRow[row] = 1;
-                LowerDiagonal[row+col] = 1;
-                UpperDiagonal[n-1+col-row] = 1;
-                Solve(col+1,board,ans,leftRow,LowerDiagonal,UpperDiagonal,n);
+                solve(col+1,board,valid,n,res,ans);
+                valid[row][col] = 0;
                 board[row][col] = '.';
-                leftRow[row] = 0;
-                LowerDiagonal[row+col] = 0;
-                UpperDiagonal[n-1+col-row] = 0;
             }
         }
+    }
+
+    public boolean checkValidOrNot(int col,int row,int n,int[][] valid){
+
+        for(int i = 0 ; i < col ; i++){
+            if(valid[row][i] == 1){
+                return false;
+            }
+        }
+
+        int i = row;
+        int j = col;
+
+        // upper diagonal
+        while(i >= 0 && j >= 0){
+            if(valid[i][j] == 1){
+                return false;
+            }
+            i--;
+            j--;
+        }
+
+        // lower diagonal
+        i = row;
+        j = col;
+
+        while(i <n && j >= 0 ){
+            if(valid[i][j] == 1){
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
     }
 
     public List<String> construct(char[][] board){
