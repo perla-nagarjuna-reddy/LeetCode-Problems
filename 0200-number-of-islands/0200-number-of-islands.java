281523ab332a04c53a1a41300f2b49a7018c8b22
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-03-01 07:28:37 +0530

    Time: 7 ms (19.29%), Space: 53.4 MB (5.66%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,58 @@
+class Pair{
+    int first;
+    int second;
+
+    public Pair(int first,int second){
+        this.first = first;
+        this.second = second;
+    }
+}
+class Solution {
+    public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int count = 0;
+        int[][] vis = new int[n][m];
+
+        for(int  i = 0 ; i < n ; i ++){
+            for(int j = 0;  j < m ; j++){
+
+                if(vis[i][j] == 0 && grid[i][j] == '1'){
+                    count++;
+                    do_bfs(i,j,grid,vis);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public void do_bfs(int row,int col,char[][] grid,int[][] vis){
+        vis[row][col] = 1;
+        Queue<int[]> q = new LinkedList<>();
+        int n = grid.length;
+        int m = grid[0].length;
+        q.add(new int[]{row,col});
+
+        while(!q.isEmpty()){
+            int[] rowCol = q.poll();
+
+            int[] dRow = {-1, 1, 0, 0}; 
+            int[] dCol = {0, 0, -1, 1}; 
+            
+            row = rowCol[0];
+
+            col = rowCol[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nRow = row + dRow[i];
+                int nCol = col + dCol[i];
+
+                if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && grid[nRow][nCol] == '1' && vis[nRow][nCol] == 0) {
+                    vis[nRow][nCol] = 1;
+                    q.add(new int[]{nRow, nCol});
+                }
+            }
+        }
+    }
+}
