15d8d406d9ce1090586e36a2e7b930286fc130b3
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-03-24 01:56:27 +0530

    Time: 4 ms (14.43%), Space: 40.8 MB (18.38%) - LeetHub

---
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long coloredCells(int n) {
+
+        long ans = 1;
+
+        for(int i = 2; i <= n ;i++){
+            ans += 4*(i - 1);
+        }
+        return ans;
+    }
+}
