e85636dfda9b1e1e4fcdd266fbb7b6ab3e63214a
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-04-02 22:49:35 +0530

    Time: 3 ms (53.95%), Space: 42.3 MB (29.21%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.java
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.java
@@ -5,9 +5,9 @@ class Solution {
 
         int n = nums.length;
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n-2; i++){
 
-            for(int j = i+1; j < n; j++){
+            for(int j = i+1; j < n-1; j++){
 
                 for(int k = j+1; k < n; k++){
 
