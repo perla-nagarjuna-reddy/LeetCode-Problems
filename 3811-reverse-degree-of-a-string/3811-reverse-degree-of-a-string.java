1a33e95e18b017abd7bae76d431139e6dacfc572
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-04-03 23:06:51 +0530

    Time: 1 ms (100%), Space: 42.2 MB (99.81%) - LeetHub

---
+++ 3811-reverse-degree-of-a-string/3811-reverse-degree-of-a-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int ans = 0;
+
+        for(int i = 0; i < s.length(); i++){
+
+            int num = (26 - (s.charAt(i) - 'a')) * (i+1);
+            ans = ans + num;
+
+        }
+
+        return ans;
+    }
+}
