3bef2cdd7c81f95ab00c841406987d8ab1a0b4ca
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-04-09 22:23:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3451-string-compression-iii/3451-string-compression-iii.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String compressedString(String word) {
+
+        int count = 1;
+
+        char prev = word.charAt(0);
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 1; i < word.length(); i++){
+
+            char current = word.charAt(i);
+
+            if(current == prev) count++;
+
+            if(count == 0){
+                count = 1;
+                prev = current;
+            }
+
+            if(current != prev){
+                sb.append(count);
+                sb.append(prev);
+                prev = current;
+                count = 1;
+            }
+            else{
+
+                if(count >= 9 && prev == current){
+                    sb.append(count);
+                    sb.append(prev);
+                    count = 0;
+                }
+            }
+            prev = current;
+        }
+        sb.append(count);
+        sb.append(prev);
+        return sb.toString();
+    }
+}
