9b377ed2a1dbb9ba2622dbc68bac7cc5c67c191a
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-04-13 13:56:53 +0530

    Time: 8 ms (66.21%), Space: 43.1 MB (24.83%) - LeetHub

---
+++ 1525-queries-on-a-permutation-with-key/1525-queries-on-a-permutation-with-key.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] processQueries(int[] queries, int m) {
+    
+        List<Integer> lst = new ArrayList<>();
+
+        int n = queries.length;
+
+        for(int i = 1; i <= m; i++){
+            lst.add(i);
+        }
+
+        int[] ans = new int[n];
+
+        for(int i = 0 ; i < queries.length; i++){
+            int num = queries[i];
+
+            int getPos = lst.indexOf(num);
+            
+            ans[i] = getPos;
+
+            int removedElement = lst.remove(getPos);
+            lst.add(0,removedElement);
+        }
+
+
+        return ans;
+    }
+}
