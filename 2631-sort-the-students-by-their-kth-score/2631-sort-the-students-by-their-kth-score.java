e2b9e4bcf3e328759af186755fba91b148518180
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-04-15 22:54:20 +0530

    Time: 2 ms (92.94%), Space: 52.5 MB (50.3%) - LeetHub

--- 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.java
+++ 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.java
@@ -1,36 +1,39 @@
-class Pair{
-    int val;
-    int pos;
+// class Pair{
+//     int val;
+//     int pos;
 
-    public Pair(int val,int pos){
-        this.val = val;
-        this.pos = pos;
-    }
-}
+//     public Pair(int val,int pos){
+//         this.val = val;
+//         this.pos = pos;
+//     }
+// }
 class Solution {
     public int[][] sortTheStudents(int[][] score, int k) {
 
-        int m = score.length;
+        // int m = score.length;
 
-        int n = score[0].length;
+        // int n = score[0].length;
 
-        int[][] ans = new int[m][n];
+        // int[][] ans = new int[m][n];
 
-        List<Pair> lst = new ArrayList<>();
+        // List<Pair> lst = new ArrayList<>();
 
-        for(int i = 0 ; i < m; i++){
-            int val = score[i][k];
-            Pair pair = new Pair(val,i);
-            lst.add(pair);
-        }
+        // for(int i = 0 ; i < m; i++){
+        //     int val = score[i][k];
+        //     Pair pair = new Pair(val,i);
+        //     lst.add(pair);
+        // }
 
-        Collections.sort(lst,(a,b) -> b.val - a.val);
+        // Collections.sort(lst,(a,b) -> b.val - a.val);
         
-        for(int i = 0 ; i < lst.size();i++){
-            Pair pair = lst.get(i);
-            ans[i] = score[pair.pos];
-        }
+        // for(int i = 0 ; i < lst.size();i++){
+        //     Pair pair = lst.get(i);
+        //     ans[i] = score[pair.pos];
+        // }
 
-        return ans;
+        // return ans;
+
+        Arrays.sort(score, (a,b)->b[k]-a[k]); 
+        return score;
     }
 }
