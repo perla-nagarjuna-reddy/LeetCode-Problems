5b0cb3b79a3063516ccbb3c9f15274122869ba0e
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-05-06 22:38:43 +0530

    Time: 0 ms (100%), Space: 41.7 MB (59.46%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,21 +10,32 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int size = sizeOfLL(head);
-        int toBeDeletedNode = size - n + 1;
+
+
+
+        int  size = sizeOfLL(head);
+
+        int deleteNode = size - n + 1;
+
         if(size == n){
             head = head.next;
             return head;
         }
-        ListNode temp = head,prev = null;
-        int cnt = 1;
+
+        int count = 1;
+
+
+        ListNode temp = head;
+
         while(temp != null){
-            if(cnt == toBeDeletedNode) break;
-            cnt++;
-            prev = temp;
+            count++;
+            if(count == deleteNode) break;
+            
             temp = temp.next;
         }
-        prev.next = prev.next.next;
+
+        temp.next = temp.next.next;
+
         return head;
     }
 
@@ -38,3 +49,31 @@ class Solution {
         return count;
     }
 }
+
+//  int size = sizeOfLL(head);
+//         int toBeDeletedNode = size - n + 1;
+//         if(size == n){
+//             head = head.next;
+//             return head;
+//         }
+//         ListNode temp = head,prev = null;
+//         int cnt = 1;
+//         while(temp != null){
+//             if(cnt == toBeDeletedNode) break;
+//             cnt++;
+//             prev = temp;
+//             temp = temp.next;
+//         }
+//         prev.next = prev.next.next;
+//         return head;
+//     }
+
+//     public int sizeOfLL(ListNode head){
+//         ListNode temp = head;
+//         int count = 0;
+//         while(temp != null){
+//             count++;
+//             temp = temp.next;
+//         }
+//         return count;
+//     }
