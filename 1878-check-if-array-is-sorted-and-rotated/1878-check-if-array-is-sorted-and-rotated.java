13243a041e0e182e807eadd264b1031374c17da7
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-05-13 23:10:45 +0530

    Time: 1 ms (6.88%), Space: 41.3 MB (51.83%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean check(int[] nums) {
         int [] B = new int[nums.length];
         int ind = 0;
-
         for(int i=1;i<nums.length;i++){
             if(nums[i] < nums[i-1]){
                 ind = i;
@@ -12,7 +11,6 @@ class Solution {
             B[i] = nums[i];
         }
         Arrays.sort(nums);
-        // System.out.println(B[(i+ind) % nums.length]);
         for(int i=0;i<nums.length;i++){
             if(nums[i] != B[(i+ind) % nums.length]){
                 return false;
