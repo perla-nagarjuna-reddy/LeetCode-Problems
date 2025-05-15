bfb563a15de91b148dd18a0828b795219b46bbb7
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-05-15 23:41:34 +0530

    Time: 1 ms (76.92%), Space: 41.6 MB (61.9%) - LeetHub

---
+++ 2391-strong-password-checker-ii/2391-strong-password-checker-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean strongPasswordCheckerII(String password) {
+
+        boolean isUpper = false;
+
+        boolean isLower = false;
+
+        boolean isDigit = false;
+
+        boolean isSpecial = false;
+
+        if(password.length() < 8) return false;
+
+        String special = "!@#$%^&*()-+";
+
+        for(int i = 0; i < password.length(); i++){
+
+            char ch = password.charAt(i);
+
+            if(i < password.length() - 1 && password.charAt(i) == password.charAt(i+1)){
+                return false;
+            }
+
+            if(!isDigit && Character.isDigit(ch)){
+                isDigit = true;
+            }
+            else if(!isUpper && Character.isUpperCase(ch)){
+                isUpper = true;
+            }
+            else if(!isLower && Character.isLowerCase(ch)){
+                isLower = true;
+            }
+            else if(!isSpecial && special.contains(String.valueOf(ch))){
+                isSpecial = true;
+            }
+        }
+
+
+
+        return isDigit && isUpper && isLower && isSpecial;
+    }
+}
