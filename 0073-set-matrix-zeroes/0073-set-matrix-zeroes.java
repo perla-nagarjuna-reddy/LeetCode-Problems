c3ab75834ca0cba757143d6ec83d486e6dffdb14
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-05-21 10:10:51 +0530

    Time: 1 ms (72.83%), Space: 45.9 MB (29.76%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,62 +1,40 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int col0 = 1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+
+        HashMap<Integer,int[]> map = new HashMap<>();
+
+        int k = 1;
+
+        for(int i = 0 ; i < matrix.length; i++){
+
+            for(int j = 0; j < matrix[0].length; j++){
+
                 if(matrix[i][j] == 0){
-                    matrix[i][0] = 0;
-                    if(j != 0){
-                        matrix[0][j] = 0;
-                    }
-                    else{
-                        col0 = 0;
-                    }
-                }
-            }
-        }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(matrix[i][j] != 0){
-                    if(matrix[0][j] == 0 || matrix[i][0] == 0){
-                        matrix[i][j] = 0;
-                    }
+
+                    map.put(k++,new int[]{i,j});
                 }
             }
         }
 
-        if(matrix[0][0] == 0){
-            for(int j=0;j<n;j++){
-                matrix[0][j] = 0;
+        for(int[] arr : map.values()){
+
+            int row = arr[0];
+
+            int col = arr[1];
+
+            int rowLength = matrix.length;
+
+            int colLength = matrix[0].length;
+
+            for(int i=0; i < colLength; i++){
+
+                matrix[row][i] = 0;
             }
-        }
-        if(col0 == 0){
-            for(int j=0;j<m;j++){
-                matrix[j][0] = 0;
+
+            for(int i=0; i < rowLength; i++){
+
+                matrix[i][col] = 0;
             }
         }
     }
 }
-
-
-// int m = matrix.length;
-//         int n = matrix[0].length;
-//         int[] row = new int[m];
-//         int[] col = new int[n];
-
-//         for(int i=0;i<m;i++){
-//             for(int j=0;j<n;j++){
-//                 if(matrix[i][j] == 0){
-//                     row[i] = 1;
-//                     col[j] = 1;
-//                 }
-//             }
-//         }
-//         for(int i=0;i<m;i++){
-//             for(int j=0;j<n;j++){
-//                 if(row[i]==1 || col[j] == 1){
-//                     matrix[i][j] = 0;
-//                 }
-//             }
-//         }
