618c52fba9d6716c5d910e4fe9461dab9dedd66b
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-06-05 01:23:34 +0530

    Time: 0 ms (100%), Space: 41.5 MB (51.2%) - LeetHub

---
+++ 1920-determine-color-of-a-chessboard-square/1920-determine-color-of-a-chessboard-square.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean squareIsWhite(String coordinates) {
+
+        HashSet<Character> set = new HashSet<>();
+
+        set.add('a');
+        set.add('c');
+        set.add('e');
+        set.add('g');
+
+
+        char ch = coordinates.charAt(0);
+
+        if(set.contains(ch)){
+
+            int num = coordinates.charAt(1) - '0';
+
+            if(num % 2 == 1) return false;
+
+            return true;
+        }
+        else{
+
+            int num = coordinates.charAt(1) - '0';
+
+            if(num % 2 == 1) return true;
+        }
+
+        return false;
+
+
+
+        // for(char ch = 'a'; ch <= 'h'; ch++){
+
+        //     if(ch == 'a' || ch == 'c' || ch =='e' || ch == 'g'){
+
+        //         int num = coordinates.charAt(1) - '0';
+
+        //         if(num % 2 == 1) return false;
+        //     }
+        // }
+    }
+}
