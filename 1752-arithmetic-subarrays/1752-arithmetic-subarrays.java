9e2206edd2a87a3ab7d09b2b317633b7006d90ae
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-07-11 01:19:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1752-arithmetic-subarrays/1752-arithmetic-subarrays.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        
+        List<Boolean> ans = new ArrayList<>();
+
+
+        for(int i = 0; i < l.length; i++){
+
+            int startInd = l[i];
+
+            int endInd = r[i];
+
+            List<Integer> lst = new ArrayList<>();
+
+            for(int j = startInd; j <= endInd; j++){
+                lst.add(nums[j]);
+            }
+            Collections.sort(lst);
+
+            int diff = lst.get(1) - lst.get(0);
+
+            boolean isArithmetic = true;
+
+            for(int k = 2; k < lst.size() - 1; k++){
+                int diffOne = Math.abs(lst.get(k) - lst.get(k+1));
+
+                if(diff != diffOne){
+                    isArithmetic = false;
+                    break;
+                }
+            }
+
+            if(isArithmetic){
+                ans.add(true);
+            }
+            else{
+                ans.add(false);
+            }
+        }
+
+        return ans;
+    }
+}
