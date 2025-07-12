2296298842062a3ccbde193c4ecd476e1fc59c91
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-07-12 13:37:52 +0530

    Time: 498 ms (11.2%), Space: 235.8 MB (5.02%) - LeetHub

---
+++ 3707-find-the-most-common-response/3707-find-the-most-common-response.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String findCommonResponse(List<List<String>> responses) {
+
+        HashMap<String,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < responses.size(); i++){
+
+            Set<String> set = new HashSet<>();
+
+            for(int j = 0; j < responses.get(i).size(); j++){
+                String st = responses.get(i).get(j);
+                if(!set.contains(st)){
+                    map.put(st,map.getOrDefault(st,0)+1);
+                }
+                set.add(st);
+            } 
+        }
+
+        System.out.println(map);
+
+        List<Map.Entry<String,Integer>> lst = new ArrayList<>(map.entrySet());
+
+        lst.sort((a,b) ->{
+
+            int freq = b.getValue().compareTo(a.getValue());
+
+            if(freq == 0){
+                return a.getKey().compareTo(b.getKey());
+            }
+
+            return freq;
+        });
+
+        return lst.get(0).getKey();
+    }
+}
