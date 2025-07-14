25141589b3b13bed324dd4b7c3181206c6197da8
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-07-14 23:41:43 +0530

    Time: 4 ms (90.41%), Space: 45.4 MB (92.47%) - LeetHub

---
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[][] sortMatrix(int[][] grid) {
+        int n = grid.length;
+        // List<List<Integer>> fin = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            List<Integer> lst = new ArrayList<>();
+            int k = 0;
+            for (int j = i;j < n ; j++) {
+                lst.add(grid[j][k++]);
+            }
+            Collections.sort(lst);
+            Collections.reverse(lst);
+            k = 0;
+            int idx = 0;
+            for (int j = i;j < n ; j++) {
+                grid[j][k++] = lst.get(idx++);
+            }
+        }
+
+        List<List<Integer>> fin1 = new ArrayList<>();
+        for (int i = 1; i < n; i++) {
+            List<Integer> lst = new ArrayList<>();
+            int row = 0, col = i;
+            while (row < n && col < n) {
+                lst.add(grid[row++][col++]);
+            }
+            Collections.sort(lst);
+            row = 0;
+            col = i;
+            int idx = 0;
+            while (row < n && col < n) {
+                grid[row++][col++] = lst.get(idx++);
+            }
+        }
+        return grid;
+        
+    }
+}
