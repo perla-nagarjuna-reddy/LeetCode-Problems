9858b959a3f134d069354e36040aab0f3c09dbf3
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-07-20 16:52:36 +0530

    Time: 10 ms (85.59%), Space: 45.6 MB (9.98%) - LeetHub

---
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int numberOfBeams(String[] bank) {
+
+        int[] arr = new int[bank.length];
+        
+        for (int i = 0; i < bank.length; i++){
+
+            int count = 0;
+
+            for(char ch : bank[i].toCharArray()){
+
+                if(ch == '1'){
+                    count++;
+                }
+            }
+            arr[i] = count;
+        }
+
+        int prev = 0;
+
+        int sum = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == 0){
+                continue;
+            }
+            else{
+                sum = sum + (prev * arr[i]);
+                prev = arr[i];
+            }
+        }
+
+        return sum;
+    }
+}
