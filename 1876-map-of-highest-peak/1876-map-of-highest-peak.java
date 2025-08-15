daf84e906f161755f30311e7bf8e7019bde6b0b9
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-08-15 10:50:22 +0530

    Time: 50 ms (57.63%), Space: 169.3 MB (22.4%) - LeetHub

---
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[][] highestPeak(int[][] isWater) {
+        Queue<int[]> q = new LinkedList<>();
+
+        int n = isWater.length;
+        int m = isWater[0].length;
+        
+        int[][] ans = new int[n][m];
+        
+        int[][] vis = new int[n][m];
+
+        for(int i = 0; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(isWater[i][j] == 1){
+                    q.add(new int[]{i,j,0});
+                    vis[i][j] = 1;
+                }
+                else{
+                    vis[i][j] = 0;
+                }
+            }
+        }
+        
+        int[] dRow = {-1,0,1,0};
+        int[] dCol = {0,1,0,-1};
+
+        while(!q.isEmpty()){
+            int[] arr = q.poll(); 
+            int row = arr[0];
+            int col = arr[1];
+            int dist = arr[2];
+            ans[row][col] = dist;
+            for(int i = 0; i < 4 ; i++){
+                int nRow = row + dRow[i];
+                int nCol = col + dCol[i];
+        
+                if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && vis[nRow][nCol] == 0){
+                    vis[nRow][nCol] = 1;
+                    q.add(new int[]{nRow,nCol,dist+1});
+                }
+            } 
+        }
+        return ans;
+    }
+}
