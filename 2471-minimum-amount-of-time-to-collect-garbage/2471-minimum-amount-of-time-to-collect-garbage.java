4edf32bc36b55a3b654aa1c820ebf046b24b10e4
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-08-17 15:06:05 +0530

    Time: 45 ms (15.99%), Space: 61 MB (40.77%) - LeetHub

---
+++ 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int garbageCollection(String[] garbage, int[] travel) {
+
+        int[] preTime = new int[travel.length + 1];
+
+        for(int i = 1; i <= travel.length;i++){
+            preTime[i] = preTime[i-1] + travel[i-1];
+        }
+
+        int totalTime = 0;
+        
+        totalTime+=calculateTimeForCollecting(garbage,preTime,'G');
+        totalTime+=calculateTimeForCollecting(garbage,preTime,'M');
+        totalTime+=calculateTimeForCollecting(garbage,preTime,'P');
+
+        return totalTime;
+    }
+
+    public static int calculateTimeForCollecting(String[] garbage, int[] preTime, char type){
+
+        int count = 0;
+
+        int index = -1;
+
+        for(int i = 0; i < garbage.length; i++){
+
+            for(char ch : garbage[i].toCharArray()){
+
+                if(ch == type){
+                    count++;
+                    index = i;
+                }
+            }
+        }
+
+        if(index == -1) return 0;
+
+        return count + preTime[index];
+    }
+}
