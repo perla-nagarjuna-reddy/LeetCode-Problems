75f02123868b35c940f8a0e30387eadc2a50c7e2
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-08-26 00:48:27 +0530

    Time: 6 ms (31.71%), Space: 48.5 MB (6.04%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[] res = new int[m * n];
+        int ind = 0;
+        
+        for (int i = 0; i < m; i++) {
+            int col = 0;
+            int row = i;
+            ArrayList<Integer> ans = new ArrayList<>();
+
+            while (row >= 0 && col < n) {
+                ans.add(mat[row][col]);
+                col++;
+                row--;
+            }
+
+            if (i % 2 == 1) {
+                Collections.reverse(ans);
+            }
+
+            for (int val : ans) {
+                res[ind++] = val;
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+           int row = m - 1, col = i;
+            ArrayList<Integer> ans = new ArrayList<>();
+            while (row >= 0 && col < n) {
+                ans.add(mat[row][col]);
+                row--; col++;
+            }
+            if ((i + m - 1) % 2 == 1) Collections.reverse(ans);
+
+            for (int val : ans) res[ind++] = val;
+        }
+
+        return res;
+    }
+}
