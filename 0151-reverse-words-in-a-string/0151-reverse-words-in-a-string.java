54dac8167d917347b8f9375edf28ef6839ca5f83
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-09-09 00:09:16 +0530

    Time: 10 ms (21.11%), Space: 45.6 MB (6.62%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -6,8 +6,6 @@ class Solution {
         for(int i=ans.length-1;i>=0;i--){
             String word = ans[i];
             if(!word.isEmpty()) val = val + word+" ";
-            // System.out.println(word);
-            // if() ;
         }
 
         return val.trim();
