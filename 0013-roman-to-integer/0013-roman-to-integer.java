6420004f310b371d7fada5518ab7b99272978ba3
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-09-22 23:36:57 +0530

    Time: 9 ms (5.14%), Space: 46 MB (6.06%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -11,43 +11,42 @@ class Solution {
         int sum = 0;
         for(int i=0;i<s.length();i++){
             String val = String.valueOf(s.charAt(i));
-            System.out.println(val);
+
             if(i < s.length()-1 && String.valueOf(s.charAt(i)).equals("I") && String.valueOf(s.charAt(i+1)).equals("V")){
                 sum+=4;
                 i+=1;
-                // System.out.println(val);
+
             }
 
             else if(i < s.length()-1 && String.valueOf(s.charAt(i)).equals("I") && String.valueOf(s.charAt(i+1)).equals("X")){
-                // System.out.println(val);
+   
                 sum+=9;
                 i+=1;
             }
             else if(i < s.length()-1 && String.valueOf(s.charAt(i)).equals("X") && String.valueOf(s.charAt(i+1)).equals("L")){
-                // System.out.println(val);
+            
                 sum+=40;
                 i+=1;
             }
             else if(i < s.length()-1 && String.valueOf(s.charAt(i)).equals("X") && String.valueOf(s.charAt(i+1)).equals("C")){
-                // System.out.println(val);
+          
                 sum+=90;
                 i+=1;
             }
             else if(i < s.length()-1 && String.valueOf(s.charAt(i)).equals("C") && String.valueOf(s.charAt(i+1)).equals("D")){
-                // System.out.println(val);
+               
                 sum+=400;
                 i+=1;
             }
             else if(i < s.length()-1 && String.valueOf(s.charAt(i)).equals("C") && String.valueOf(s.charAt(i+1)).equals("M")){
-                // System.out.println(val);
+
                 sum+=900;
-                // System.out.println(sum +" " + val);
+
                 i+=1;
             }
             else{
                 
                 sum+=map.get(val);
-                // System.out.println(val);
             }
            
 
