c361956386803fd61747d5d0fa2add61c913b119
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-10-03 01:08:37 +0530

    Time: 0 ms (100%), Space: 41.1 MB (30.83%) - LeetHub

---
+++ 3336-water-bottles-ii/3336-water-bottles-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+
+        int drunk = numBottles;
+
+        int empty = numBottles;
+
+
+        while(empty >= numExchange){
+
+            int newB = 1;
+            drunk+=newB;
+            empty = empty - numExchange + 1;
+            numExchange++;
+        }
+        return drunk;
+    }
+}
