f80a2ce64f0242f4e881dafd85b7d19b5a4a8019
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-10-03 01:16:02 +0530

    Time: 0 ms (100%), Space: 41.1 MB (41.1%) - LeetHub

--- 3336-water-bottles-ii/3336-water-bottles-ii.java
+++ 3336-water-bottles-ii/3336-water-bottles-ii.java
@@ -9,9 +9,9 @@ class Solution {
         while(empty >= numExchange){
             int newB = 1;
             drunk+=newB;
-            empty = empty - numExchange + 1;
+            empty = empty - numExchange + 1; // to add into emtpy bottle list of new full bottle
             numExchange++;
-        }
+        }     
         return drunk;
     }
 }
