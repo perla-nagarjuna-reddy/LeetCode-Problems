47bc728d87c54e5fea926cda9869ce8b33b2ca15
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-10-06 02:09:50 +0530

    Time: 10 ms (10.98%), Space: 61.9 MB (5.49%) - LeetHub

---
+++ 2872-largest-element-in-an-array-after-merge-operations/2872-largest-element-in-an-array-after-merge-operations.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long maxArrayValue(int[] nums) {
+
+        long sum = 0;
+
+        long[] arr = Arrays.stream(nums)
+                            .asLongStream()
+                            .toArray();
+
+
+        for(int  i = arr.length - 1; i >= 0 ; i--){
+
+            if(i-1 >= 0 && arr[i-1] <= arr[i]){
+                arr[i-1]+=arr[i];
+            }
+        }
+
+        for(long ele : arr){
+            sum = Math.max(sum,ele);
+        }
+
+        return sum;
+    }
+}
