2b16e7fc47be8334f839270b62f4d498209d80d7
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-10-06 02:29:26 +0530

    Time: 1 ms (100%), Space: 57.6 MB (79.27%) - LeetHub

--- 2872-largest-element-in-an-array-after-merge-operations/2872-largest-element-in-an-array-after-merge-operations.java
+++ 2872-largest-element-in-an-array-after-merge-operations/2872-largest-element-in-an-array-after-merge-operations.java
@@ -3,15 +3,15 @@ class Solution {
 
         long ans = nums[nums.length - 1];
 
-        long res = 0;
-
         for (int i = nums.length - 2; i >= 0; i--) {
+            
             if (nums[i] <= ans) {
+
                 ans += nums[i];
+
             } else {
                 ans = nums[i];
             }
-            res = Math.max(res,ans);
         }
 
         return ans;
