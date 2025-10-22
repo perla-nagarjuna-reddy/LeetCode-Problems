94a301c5eb8844c85b2b78b72527ba802e24d7d4
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-10-22 23:17:46 +0530

    Time: 2 ms (43.87%), Space: 43.4 MB (34.51%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,28 +1,27 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
-        int open = n;
-		int close = n;
-		solve(open,close,"",ans);
+
+		solve(n,0,0,"",ans);
         return ans;
     }
 
-    public static void solve(int open,int close,String op,List<String> ans){
-	    if(open == 0 && close == 0){
-	        ans.add(new String(op));
-	        return;
-	    }
-	    
-	    if(open != 0){
-	        String op1 = op;
-	        op1 = op1 + "(";
-	        solve(open-1,close ,op1,ans);
-	    }
-	    
-	    if(close > open){
-	       String op2 = op;
-	       op2 = op2 + ")";
-	       solve(open,close-1 ,op2,ans);
-	    }
+    public static void solve(int n,int open,int close,String op,List<String> ans){
+
+        if(open == n && close == n){
+            ans.add(new String(op));
+            return;
+        }
+
+        if(open < n){
+            String op1 = op;
+            op1 = op1 + '(';
+            solve(n,open+1,close,op1,ans);
+        }
+        if(close < open){
+            String op2 = op;
+            op2 = op2 + ')';
+            solve(n,open,close + 1,op2,ans);
+        }
 	}
 }
