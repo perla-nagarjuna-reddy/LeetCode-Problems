800f7d91518a1ad59fc7f1072be606a0e1a8e2ed
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-10-26 23:07:38 +0530

    Time: 4 ms (8.23%), Space: 43.9 MB (14.54%) - LeetHub

---
+++ 1674-minimum-operations-to-make-array-equal/1674-minimum-operations-to-make-array-equal.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minOperations(int n) {
+        
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = (2*i) + 1;
+        }
+
+
+        int median = arr[n / 2];
+
+        int ops = 0;
+
+        for(int i = 0; i < n; i++){
+
+            ops+=Math.abs(median - arr[i]) / 2;
+        }
+
+        return ops;
+
+    }
+}
