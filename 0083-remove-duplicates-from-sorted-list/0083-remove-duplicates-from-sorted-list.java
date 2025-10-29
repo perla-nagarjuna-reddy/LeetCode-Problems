7b7fce187848b7fef9d271fc33a5af29e3ec1b32
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-10-30 00:17:58 +0530

    Time: 0 ms (100%), Space: 44.2 MB (58.87%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -10,50 +10,22 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-
-        if(head == null || head.next == null) return head;
-        ListNode temp = head; // Initialize the pointer to traverse the list
         
-        while (temp.next != null) { // Continue until the end of the list
-            if (temp.val == temp.next.val) { // Check if current node is a duplicate
-                temp.next = temp.next.next; // Skip the duplicate node
-            } else {
-                temp = temp.next; // Move to the next node
+        ListNode temp = head;
+
+        while(temp != null){
+
+            ListNode t1 = temp;
+
+            while(t1 != null && t1.next != null && t1.val == t1.next.val){
+                if(t1.next != null){
+                    t1.next = t1.next.next;
+                }
             }
+
+            temp = temp.next;
         }
-        
-        return head; // Return the modified list
+
+        return head;
     }
-       
 }
-
-
-//  ArrayList<Integer> arr = new ArrayList<>();
-
-//         ListNode temp = head;
-
-//         while(temp != null){
-//             if(arr.contains(temp.val)){
-//                 temp = temp.next;
-//                 continue;
-//             }
-//             else{
-//                 arr.add(temp.val);
-//             }
-//             temp = temp.next;
-//         }
-
-//         return createLL(arr);
-//     }
-
-//     public ListNode createLL(ArrayList<Integer> arr){
-//         ListNode head = new ListNode(arr.get(0));
-//         ListNode mover = head;
-//         for(int i=1;i<arr.size();i++){
-//             ListNode temp = new ListNode(arr.get(i));
-//             mover.next = temp;
-//             mover = temp;
-//         }
-
-//         return head;
-//     }
