00eaeec4df54257d9c45e98b563434cb35e82b04
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-10-29 23:56:44 +0530

    Time: 0 ms (100%), Space: 40.5 MB (87.17%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
@@ -1,36 +1,12 @@
 class Solution {
     public int smallestNumber(int n) {
 
-        int start = n; 
-
-        while(true){
-
-            List<Integer> lst = new ArrayList<>();
-
-            int temp = start;
-
-            boolean isBitSet = true;
-
-            while(temp != 1){
-                
-                lst.add(temp % 2);
-
-                temp = temp / 2;
-            }
-            for(int val : lst){
-
-                if(val == 0){
-                    isBitSet = false;
-                }
-            }
-
-            if(isBitSet){
-                break;
-            }
-
-            start++;
+        int num=n;
+        
+        while((num&(num+1))!=0){
+            num++;
         }
 
-        return start;
+        return num;
     }
 }
