33b0c25a9020f2a5acc7c7bbde3cdfdd870969a0
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-11-07 02:15:37 +0530

    Time: 1 ms (25.7%), Space: 42.3 MB (7.61%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < 32; i++) {
             result <<= 1;        
             result |= (n & 1);   
-            n >>>= 1;            
+            n >>= 1;            
         }
         return result;
     }
