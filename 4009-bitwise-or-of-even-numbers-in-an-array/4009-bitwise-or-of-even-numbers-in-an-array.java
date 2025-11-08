71226bd23a93872f60072d5d428679146e74f1fe
Perla Nagarjuna Reddy <115425980+perla-nagarjuna-reddy@users.noreply.github.com>
2025-11-09 00:19:22 +0530

    Time: 0 ms (100%), Space: 44.6 MB (5.88%) - LeetHub

--- 4009-bitwise-or-of-even-numbers-in-an-array/4009-bitwise-or-of-even-numbers-in-an-array.java
+++ 4009-bitwise-or-of-even-numbers-in-an-array/4009-bitwise-or-of-even-numbers-in-an-array.java
@@ -5,7 +5,7 @@ class Solution {
 
         for(int i = 0; i < nums.length; i++){
 
-            if(nums[i] % 2 == 0){
+            if((nums[i] & 1) == 0){
                 ans = ans | nums[i];
             }
         }
